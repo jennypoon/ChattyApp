fc6f560500f61e9ff9aa041bfdf4c9f85e60bc7e
Jenny Poon <jenny.poon@live.ca>
2018-10-30 19:46:42 +0000

    access to props currentUser

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,18 +1,30 @@
 import React, {Component} from 'react';
 
-function ChatBar() {
-  return(
-    <footer className="chatbar">
-      <input
-        className="chatbar-username"
-        placeholder="Your Name (Optional)"
-        />
-      <input
-        className="chatbar-message"
-        placeholder="Type a message and hit ENTER"
+class ChatBar extends Component {
+
+ constructor(props) {
+    super(props);
+
+    // this.state = {
+    //   username: this.props.currentUser
+    //   }
+    }
+
+  render() {
+    return(
+      <footer className="chatbar">
+        <input
+          className="chatbar-username"
+          placeholder="Your Name (Optional)"
+          defaultValue={this.props.currentUser}
+          />
+        <input
+          className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
         />
     </footer>
     )
+  }
 }
 
 export default ChatBar;
