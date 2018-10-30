7e7db42deb0bcb10fff7f699672d0efebb91d086
Jenny Poon <jenny.poon@live.ca>
2018-10-30 19:47:44 +0000

    created static data, created state and passed data to children via props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
-import Message from './Message.jsx';
+
+
+
+const data =
+{
+  "currentUser": {"name": "Bob"},
+  "messages": [
+    {
+      "username": "Bob",
+      "content": "Has anyone seen my marbles?"
+    },
+    {
+      "username": "Anonymous",
+      "content": "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+}
 
 
 function Navbar() {
@@ -13,13 +29,20 @@ function Navbar() {
 }
 
 
-class App extends React.Component {
+class App extends Component {
+
+constructor(props) {
+    super(props);
+
+    this.state = { data }
+}
+
   render() {
     return (
       <div>
         <Navbar />
-        <Message/>
-        <ChatBar />
+        <MessageList currentMessage={ this.state.data.messages }/>
+        <ChatBar currentUser={ this.state.data.currentUser.name } />
       </div>
     )
   }
