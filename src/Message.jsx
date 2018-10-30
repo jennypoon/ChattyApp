ba40c85b0106a69b328f4673d9bc23f82fc86265
Jenny Poon <jenny.poon@live.ca>
2018-10-30 20:14:38 +0000

    added if statement so notifications are treated differentlyl

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 function Message(props) {
+console.log(props)
+  if(props.type === "incomingMessage") {
 
-  return (
-    <div className="message">
-      <span className="message-username">{props.user}</span>
-      <span className="message-content">{props.content}</span>
-    </div>
+    return (
+      <div className="message">
+        <span className="message-username">{props.user}</span>
+        <span className="message-content">{props.content}</span>
+      </div>
     )
+
+  } else {
+
+    return (
+      <div className="message system">
+        {props.content}
+      </div>
+    )
+  }
 }
 
 export default Message;
