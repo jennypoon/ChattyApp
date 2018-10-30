4a656f2b5fbc0c18ee3f129222de55b8608bc8d6
Jenny Poon <jenny.poon@live.ca>
2018-10-30 21:31:34 +0000

    componentdidmount() function added for hard codied message

--- src/App.jsx
+++ src/App.jsx
@@ -19,35 +19,35 @@ const data =
     type: "incomingNotification",
     content: "Anonymous1 changed their name to nomnom",
   },
-  {
-    id: 3,
-    type: "incomingMessage",
-    content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-    username: "Anonymous2"
-  },
-  {
-    id: 4,
-    type: "incomingMessage",
-    content: "...",
-    username: "nomnom"
-  },
-  {
-    id: 5,
-    type: "incomingMessage",
-    content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
-    username: "Anonymous2"
-  },
-  {
-    id: 6,
-    type: "incomingMessage",
-    content: "This isn't funny. You're not funny",
-    username: "nomnom"
-  },
-  {
-    id: 7,
-    type: "incomingNotification",
-    content: "Anonymous2 changed their name to NotFunny",
-  },
+  // {
+  //   id: 13,
+  //   type: "incomingMessage",
+  //   content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
+  //   username: "Anonymous2"
+  // },
+  // {
+  //   id: 4,
+  //   type: "incomingMessage",
+  //   content: "...",
+  //   username: "nomnom"
+  // },
+  // {
+  //   id: 5,
+  //   type: "incomingMessage",
+  //   content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
+  //   username: "Anonymous2"
+  // },
+  // {
+  //   id: 6,
+  //   type: "incomingMessage",
+  //   content: "This isn't funny. You're not funny",
+  //   username: "nomnom"
+  // },
+  // {
+  //   id: 7,
+  //   type: "incomingNotification",
+  //   content: "Anonymous2 changed their name to NotFunny",
+  // },
   ]
 
 }
@@ -67,15 +67,34 @@ class App extends Component {
 constructor(props) {
     super(props);
 
-    this.state = { data }
+    this.state = {
+      currentUser: data.currentUser,
+      messages: data.messages
+    }
 }
 
+componentDidMount() {
+  console.log("componentDidMount <App />");
+  setTimeout(() => {
+    console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    const newMessage = {id: 3, username: "Michelle", content: "Hello there!", type: "incomingMessage" };
+    const newMessages = this.state.messages.concat(newMessage)
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.
+    this.setState({messages: newMessages})
+
+  }, 3000);
+}
+
+
   render() {
+    console.log("this current state", this.state)
     return (
       <div>
         <Navbar />
-        <MessageList currentMessage={ this.state.data.messages }/>
-        <ChatBar currentUser={ this.state.data.currentUser.name } />
+        <MessageList currentMessage={ this.state.messages }/>
+        <ChatBar currentUser={ this.state.currentUser.name } />
       </div>
     )
   }
