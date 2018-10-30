c6c9763109bf19fd104d545d3c9d43f66d301b9b
Jenny Poon <jenny.poon@live.ca>
2018-10-30 05:31:09 +0000

    created class and imported Notification

---
+++ src/MessageList.jsx
@@ -0,0 +1,11 @@
+import Notification from './Notification.jsx';
+import Message from './Message.jsx';
+import React, { Component } from 'react';
+
+class MessageList extends Component {
+  render() {
+    return ();
+  }
+}
+
+export default MessageList;
