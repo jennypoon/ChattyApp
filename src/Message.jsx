13332cd9824632c00671260ee52ead3e6f34f8ee
Jenny Poon <jenny.poon@live.ca>
2018-11-01 23:12:15 +0000

    inline styling for user

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 function Message(props) {
+  let style ={
+    color: props.color
+  }
+
+  console.log("message props", style)
   switch(props.type) {
 
     case("incomingMessage"):
       return(
         <div className="message">
-          <span className="message-username">{props.user}</span>
+          <span className="message-username" style={style}>{props.user}</span>
           <span className="message-content">{props.content}</span>
         </div>
         )
