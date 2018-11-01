264473c8cea5d686113258318fb517a513ee03f3
Jenny Poon <jenny.poon@live.ca>
2018-11-01 22:10:16 +0000

    removed User left case to let server handle who has left chat

--- src/App.jsx
+++ src/App.jsx
@@ -3,71 +3,13 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
 
-// const data =
-// {
-//   "currentUser": {"name": "Bob"},
-//   "messages": [
-//   {
-//     id: 1,
-//     type: "incomingMessage",
-//     content: "I won't be impressed with technology until I can download food.",
-//     username: "Anonymous1"
-//   },
-//   {
-//     id: 2,
-//     type: "incomingNotification",
-//     content: "Anonymous1 changed their name to nomnom",
-//   },
-//   // {
-//   //   id: 13,
-//   //   type: "incomingMessage",
-//   //   content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-//   //   username: "Anonymous2"
-//   // },
-//   // {
-//   //   id: 4,
-//   //   type: "incomingMessage",
-//   //   content: "...",
-//   //   username: "nomnom"
-//   // },
-//   // {
-//   //   id: 5,
-//   //   type: "incomingMessage",
-//   //   content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
-//   //   username: "Anonymous2"
-//   // },
-//   // {
-//   //   id: 6,
-//   //   type: "incomingMessage",
-//   //   content: "This isn't funny. You're not funny",
-//   //   username: "nomnom"
-//   // },
-//   // {
-//   //   id: 7,
-//   //   type: "incomingNotification",
-//   //   content: "Anonymous2 changed their name to NotFunny",
-//   // },
-//   ]
-
-// }
-
-
-function Navbar() {
-  return (
-    <nav className="navbar">
-      <a href="/" className="navbar-brand">Chatty</a>
-    </nav>
-  )
-}
-
-
 class App extends Component {
 
 constructor(props) {
     super(props);
 
     this.state = {
-      currentUser: {name: "Anonymous"}, //data.currentUser,
+      currentUser: {name: "Anonymous"},
       messages: [],
       usersConnected: 0
     };
@@ -81,7 +23,6 @@ addMessage(message, user) {
   console.log("Messaged recieved!")
 
   const newMessage = {
-    // id: this.state.messages.length + 1,
     type: "incomingMessage",
     content: message,
     username: user
@@ -92,12 +33,13 @@ addMessage(message, user) {
 }
 
 changeUser(user) {
-  console.log("Change User - user:", user)
+  // console.log("Change User - user:", user)
   this.setState({currentUser: {name: user}})
 
   this.socket.send(JSON.stringify({
     type: 'incomingNotification',
-    content: this.state.currentUser.name + ' has changed their name to ' + user
+    content: this.state.currentUser.name + ' has changed their name to ' + user,
+    newName: user
   }))
 
 }
@@ -114,15 +56,26 @@ componentDidMount() {
 
     let wsEvent = JSON.parse(message.data);
 
-    if (wsEvent.type === "totalUserValue"){
-      console.log("you got totalUserValue!", wsEvent.content )
-      this.setState({usersConnected: wsEvent.content})
+    switch(wsEvent.type) {
 
-    } else {
-    console.log("message from WS Server", wsEvent)
+      case ("totalUserValue"):
+        // const newUser = {
+        //   type: "incomingNotification",
+        //   content: `${this.state.currentUser.name} has joined`
+        // }
 
-    const newMessages = this.state.messages.concat(wsEvent)
-    this.setState({messages: newMessages})
+        // const newMsg = this.state.messages.concat(newUser)
+
+        this.setState({
+          usersConnected: wsEvent.content,
+          // messages: newMsg
+        })
+        break;
+
+      default:
+        // For Notifications and New Messages
+        const newMessages = this.state.messages.concat(wsEvent)
+        this.setState({messages: newMessages})
     }
   }
 }
