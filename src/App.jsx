9d4ecbed42a10c420a679b39365c2a3a2e2ca81c
Jenny Poon <jenny.poon@live.ca>
2018-11-01 23:11:12 +0000

    added colour state to constructor and passed to message

--- src/App.jsx
+++ src/App.jsx
@@ -5,81 +5,80 @@ import MessageList from './MessageList.jsx';
 
 class App extends Component {
 
-constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
       currentUser: {name: "Anonymous"},
       messages: [],
-      usersConnected: 0
+      usersConnected: 0,
+      color: "000000"
     };
 
     this.addMessage = this.addMessage.bind(this)
     this.changeUser = this.changeUser.bind(this)
-
-}
-
-addMessage(message, user) {
-  console.log("Messaged recieved!")
-
-  const newMessage = {
-    type: "incomingMessage",
-    content: message,
-    username: user
   }
 
-  this.socket.send(JSON.stringify(newMessage));
+  addMessage(message, user) {
+    // console.log("Messaged received!")
 
-}
+    const newMessage = {
+      type: "incomingMessage",
+      content: message,
+      username: user
+    }
 
-changeUser(user) {
-  // console.log("Change User - user:", user)
-  this.setState({currentUser: {name: user}})
+    this.socket.send(JSON.stringify(newMessage));
+  }
 
-  this.socket.send(JSON.stringify({
-    type: 'incomingNotification',
-    content: this.state.currentUser.name + ' has changed their name to ' + user,
-    newName: user
-  }))
+  changeUser(user) {
+    // console.log("Change User - user:", user)
+    this.setState({currentUser: {name: user}})
 
-}
+    this.socket.send(JSON.stringify({
+      type: 'incomingNotification',
+      content: this.state.currentUser.name + ' has changed their name to ' + user,
+      newName: user
+    }))
+  }
 
-componentDidMount() {
-  this.socket = new WebSocket("ws://localhost:3001/");
+  componentDidMount() {
+  //Create a new socket
+    this.socket = new WebSocket("ws://localhost:3001/");
 
-  this.socket.onopen = (event) => {
-    console.log("Connected to server");
-  };
+  //Once Socket opens from server
+    this.socket.onopen = (event) => {
+      console.log("Connected to server");
+    };
 
-  this.socket.onmessage = (message) => {
-    console.log("Socket message", message)
+  //Receives Server Messages
+    this.socket.onmessage = (message) => {
+      console.log("Socket message", message)
 
-    let wsEvent = JSON.parse(message.data);
+      let wsEvent = JSON.parse(message.data);
 
-    switch(wsEvent.type) {
+      switch(wsEvent.type) {
 
-      case ("totalUserValue"):
-        // const newUser = {
-        //   type: "incomingNotification",
-        //   content: `${this.state.currentUser.name} has joined`
-        // }
+        case ("totalUserValue"):
+          this.setState({
+            usersConnected: wsEvent.content,
+          })
+          break;
 
-        // const newMsg = this.state.messages.concat(newUser)
+        case ("userColour"):
+          console.log("usercolour", wsEvent)
+          this.setState({
+            color: wsEvent.content,
+          })
+          break;
 
-        this.setState({
-          usersConnected: wsEvent.content,
-          // messages: newMsg
-        })
-        break;
-
-      default:
-        // For Notifications and New Messages
-        const newMessages = this.state.messages.concat(wsEvent)
-        this.setState({messages: newMessages})
+        default:
+          // For Notifications and New Messages
+          const newMessages = this.state.messages.concat(wsEvent)
+          this.setState({messages: newMessages})
+      }
     }
   }
-}
-
 
   render() {
     console.log("current state after render", this.state)
@@ -89,7 +88,7 @@ componentDidMount() {
           <a href="/" className="navbar-brand">Chatty</a>
           <span className="navbar-userscount">Users connected:{ this.state.usersConnected }</span>
         </nav>
-        <MessageList currentMessage={ this.state.messages }/>
+        <MessageList color={ this.state.color } currentMessage={ this.state.messages }/>
         <ChatBar changeUser={ this.changeUser } addMessage={ this.addMessage } currentUser={ this.state.currentUser.name } />
       </div>
     )
