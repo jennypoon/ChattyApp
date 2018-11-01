3d53d51b078dcb00e73916f2a038cd3850817745
Jenny Poon <jenny.poon@live.ca>
2018-11-01 23:11:35 +0000

    passed colour prop to message

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,13 +2,16 @@ import Message from './Message.jsx';
 import React, { Component } from 'react';
 
 class MessageList extends Component {
+
   render() {
+
     const message = this.props.currentMessage.map(message => {
       return < Message
         key={message.id}
         user={message.username}
         content={message.content}
         type={message.type}
+        color={this.props.color}
         />
     })
 
