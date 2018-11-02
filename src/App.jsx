11974d1442e006b53fad78df6c1076a88281c67d
Jenny Poon <jenny.poon@live.ca>
2018-11-02 00:33:09 +0000

    added cleaned up

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ class App extends Component {
       currentUser: {name: "Anonymous"},
       messages: [],
       usersConnected: 0,
-      color: "000000"
     };
 
     this.addMessage = this.addMessage.bind(this)
@@ -20,19 +19,16 @@ class App extends Component {
   }
 
   addMessage(message, user) {
-    // console.log("Messaged received!")
 
     const newMessage = {
       type: "incomingMessage",
       content: message,
-      username: user
+      username: user,
     }
-
     this.socket.send(JSON.stringify(newMessage));
   }
 
   changeUser(user) {
-    // console.log("Change User - user:", user)
     this.setState({currentUser: {name: user}})
 
     this.socket.send(JSON.stringify({
@@ -53,8 +49,6 @@ class App extends Component {
 
   //Receives Server Messages
     this.socket.onmessage = (message) => {
-      console.log("Socket message", message)
-
       let wsEvent = JSON.parse(message.data);
 
       switch(wsEvent.type) {
@@ -65,30 +59,24 @@ class App extends Component {
           })
           break;
 
-        case ("userColour"):
-          console.log("usercolour", wsEvent)
-          this.setState({
-            color: wsEvent.content,
-          })
-          break;
-
         default:
           // For Notifications and New Messages
           const newMessages = this.state.messages.concat(wsEvent)
-          this.setState({messages: newMessages})
+          this.setState({
+            messages: newMessages,
+            })
       }
     }
   }
 
   render() {
-    console.log("current state after render", this.state)
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
           <span className="navbar-userscount">Users connected:{ this.state.usersConnected }</span>
         </nav>
-        <MessageList color={ this.state.color } currentMessage={ this.state.messages }/>
+        <MessageList currentMessage={ this.state.messages }/>
         <ChatBar changeUser={ this.changeUser } addMessage={ this.addMessage } currentUser={ this.state.currentUser.name } />
       </div>
     )
