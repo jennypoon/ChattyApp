2ee3af45ad88882b10b4357fde59f8c52aa27981
Jenny Poon <jenny.poon@live.ca>
2018-12-13 08:30:00 +0000

    fixed port typo

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3001;
 const server = express()
    // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+  .listen(port, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ port }`));
 
 // const httpServer = http.createServer(server)
 
